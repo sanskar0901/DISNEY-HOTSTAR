dd58931ccf3553dc51782b53c66bb79c5a17a203
AKSHAT20-OFFICIAL <72640097+AKSHAT20-OFFICIAL@users.noreply.github.com>
2021-07-23 23:41:07 +0530

    drawer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+ 
+} from "react-router-dom";
+
+
 import Navbar from './components/Navbar/Navbar.jsx'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
      
-     <Navbar/>  
+     <Navbar/>
+     <Router>
+       <Switch>
+          <Route  path="/Home">
+            
+          </Route>
+          <Route exact path="/about">
+            
+          </Route>
+          <Route exact path="/dashboard">
+            
+          </Route>
+        </Switch>
+     </Router>
+
+     
     </div>
   );
 }
